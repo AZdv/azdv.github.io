db11c9ae0656901872fcc07fe4e420789111705b
Asaf Zamir <asaf.zamir@gmail.com>
2017-09-18 16:23:38 +0100

    Version 1.0

---
+++ js/contact.js
@@ -0,0 +1,18 @@
+(function($) {
+	$( '#contact_form' ).ajaxForm({
+		beforeSubmit: function(formData, jqForm, options) {
+			for (var i=0; i < formData.length; i++) { 
+				var formValid = true;
+				if (!formData[i].value) { 
+					formValid = false;
+					$( '[name="' + formData[i].name + '"]' ).addClass('invalid');
+				} else {
+					$( '[name="' + formData[i].name + '"]' ).removeClass('invalid');
+				}
+			}
+			if ( ! formValid )
+				return false;
+			submitted = true;
+		},
+	})
+})(jQuery);
